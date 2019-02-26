872b766569502aa5e158d4c991e8eac40292eeac
Victor Maia <srvictormaia@gmail.com>
2019-02-26 13:58:29 -0300

    Update Formality

--- javascript/build.js
+++ javascript/build.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 var files = fs.readdirSync("./../lib");
-var codes = {};
+var code = "";
 for (var i = 0; i < files.length; ++i) {
   if (files[i].slice(-3) === ".fm") {
-    var name = files[i].slice(0, -3);
-    codes[name] = fs.readFileSync("./../lib/" + files[i], "utf8").replace(/`/g, "");
+    code += fs.readFileSync("./../lib/" + files[i], "utf8").replace(/`/g, "") + "\n";
   }
 }
 
-fs.writeFileSync("formality-stdlib.js", "module.exports = `" + JSON.stringify(codes, 0, 2) + "`;");
+fs.writeFileSync("formality-stdlib.js", "module.exports = `" + code + "`;");
